3e7222870c2788b83aae54ba687d2153c44f870d
Semen <buhtabest@gmail.com>
2026-01-19 18:33:48 +0700

    Форма регистрации синяя

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -1,2 +1,12 @@
 <?php
-echo'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazzzzzzzzzzzzzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaaaaa';
+
+print_r($_GET);
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+$passwordRepeat = $_GET['passwordRepeat'];
+
+$pdo = new PDO("pgsql:host=db;port=5432;dbname=postgres", "semen", "0000");
+
+$pdo->exec("INSERT INTO users (name, email, password) VALUES ('name', 'email', 'password','passwordRepeat')");
--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,14 @@
 <?php
 $pdo = new PDO("pgsql:host=db;port=5432;dbname=postgres", "semen", "0000");
+
 $pdo->exec("INSERT INTO users (name, email, password) VALUES ('Ivan', 'ivan@gmail.com', 'password')");
+
 $statement = $pdo->query("SELECT * FROM users");
+
 $data = $statement->fetchAll();
+echo "<pre>";
 print_r($data);
+
+
+
+
