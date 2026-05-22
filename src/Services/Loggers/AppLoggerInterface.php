b7a9c70ad5d390da7dcaff4198596e9139e2802e
Semeneriti <buhtabest@gmail.com>
2026-05-22 17:10:57 +0500

    update4

--- src/Services/Loggers/AppLoggerInterface.php
+++ src/Services/Loggers/AppLoggerInterface.php
@@ -2,9 +2,11 @@
 
 namespace Services\Loggers;
 
-interface LoggerInterface
+interface AppLoggerInterface
 {
     public function error(string $message, array $context = []): void;
+
     public function info(string $message, array $context = []): void;
+
     public function warning(string $message, array $context = []): void;
 }
